7643c903d5a19cb07616d34e609aaed74de8f814
Meng Sun <sunmeng90@126.com>
2017-06-25 12:35:41 +0800

    Sercie: change service signature to return a Promise of Hero array

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,7 +6,7 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService{
   //A Promise essentially promises to call back when the results are ready. You ask an asynchronous service to do some work and give it a callback function. The service does that work and eventually calls the function with the results or an error.
-  getHeroes(): void {
+  getHeroes(): Promise<Hero[]> {
   	return Promise.resolve(HEROES);
   }//stub
 }

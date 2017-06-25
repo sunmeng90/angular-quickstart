21cfbd7edef437306fc035ada4f4a5d108f31ada
Meng Sun <sunmeng90@126.com>
2017-06-25 17:54:13 +0800

    Route: add route to navigate to different pages 1. import RouterModule 2. config route 'path' and 'component' 	RouterModule.forRoot([   		{     			path: 'heroes',     			component: HeroesComponent   		} 	]) 3. add route-outlet:  The router displays each component immediately below the <router-outlet> as users navigate through the app. 4. Router links: The RouterLink directive (another of the RouterModule directives) is bound to a string that tells the router where to navigate when the user clicks the link. 5. Parameterized route: (The colon (:) in the path indicates that :id is a placeholder for a specific hero id when navigating to the HeroDetailComponent) 	{   		path: 'detail/:id',   		component: HeroDetailComponent 	}    parameterized route needs a ActivatedRoute and import 'rxjs/add/operator/switchMap'; 6. add router link: <a *ngFor="let hero of heroes"  [routerLink]="['/detail', hero.id]"  class="col-1-4">.    you're binding to an expression containing a link parameters array. The array has two elements: the path of the destination route and a route parameter set to the value of the current hero's id.    The two array items align with the path and :id token in the parameterized hero detail route definition that you added to app.module.ts earlier.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,58 @@
+//angular
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+//components
 import { FormsModule} from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
+//service
+import { HeroService } from './hero.service';
+
+
+//The providers array tells Angular to create a fresh instance of the HeroService when it creates an AppComponent
+//Parameterized route: path: 'detail/:id'(The colon (:) in the path indicates that :id is a placeholder for a specific hero id when navigating to the HeroDetailComponent.)
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports:      [ 
+  	BrowserModule, 
+  	FormsModule,
+  	RouterModule.forRoot([
+  		{
+  			path: '',
+  			redirectTo: '/dashboard',
+  			pathMatch: 'full'
+  		},
+  		{
+  			path: 'dashboard',
+  			component: DashboardComponent
+  		},
+  		{
+  			path: 'heroes',
+  			component: HeroesComponent
+  		},
+  		{
+  			path: 'detail/:id',
+  			component: HeroDetailComponent
+  		}
+  	])
+  ],
+  declarations: [ 
+  	AppComponent, 
+  	DashboardComponent,
+  	HeroesComponent, 
+  	HeroDetailComponent 
+
+  ],
+  providers: [ 
+  	HeroService 
+
+  ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule { 
+	title = 'Tour of Heroes';
+}
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap'; //switchMap operator
+
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'hero-detail',
-	template: `
-	  <div *ngIf="hero">
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}
-	  </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-	`
+	templateUrl: './hero-detail.component.html'
 })
 
-export class HeroDetailComponent{
+export class HeroDetailComponent implements OnInit {
 	@Input() hero: Hero;
+
+	constructor(
+		private heroServcie: HeroService;
+		private route: ActivatedRoute;
+		private location: Location;
+		
+	){}
+	//The switchMap operator maps the id in the Observable route parameters to a new Observable, the result of the HeroService.getHero() method.
+	//If a user re-navigates to this component while a getHero request is still processing, switchMap cancels the old request and then calls HeroService.getHero() again.
+	//The hero id is a number. Route parameters are always strings. So the route parameter value is converted to a number with the JavaScript (+) operator.
+	ngOnInit(): void {
+
+		this.route.params
+				.switchMap((params: Params) => this.heroServcie.getHero(+params['id']))
+				.subscribe(hero => this.hero = hero);
+	}
+
+	goBack(): void {
+		this.location.back();
+	}
 }
